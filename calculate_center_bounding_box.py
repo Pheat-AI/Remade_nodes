b750202adaff7cbaa6673d002a4033322f8d744b
Pheat-AI <info@pheat.ai>
2024-09-09 15:16:33 -0700

    Update calculate_center_bounding_box.py
    added necessary comfy components

--- calculate_center_bounding_box.py
+++ calculate_center_bounding_box.py
@@ -1,4 +1,7 @@
 class CalculateCenterFromBoundingBox:
+    def __init__(self):
+        pass 
+
     @classmethod
     def INPUT_TYPES(cls):
         return {
@@ -13,6 +16,7 @@ class CalculateCenterFromBoundingBox:
     RETURN_TYPES = ("INT", "INT")
     RETURN_NAMES = ("center_x", "center_y")
     FUNCTION = "calculate_center"
+
     CATEGORY = "essentials/calculations"
 
     def calculate_center(self, x, y, width, height):
@@ -22,3 +26,13 @@ class CalculateCenterFromBoundingBox:
         
         # Return the center coordinates
         return (center_x, center_y)
+
+# A dictionary that contains all nodes you want to export with their names
+NODE_CLASS_MAPPINGS = {
+    "Calculate Center From Bounding Box": CalculateCenterFromBoundingBox
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Calculate Center From Bounding Box": "Calculate Center from Bounding Box"
+}
